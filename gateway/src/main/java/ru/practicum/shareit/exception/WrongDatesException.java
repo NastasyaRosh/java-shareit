f07b426041f3939702f3f96cfc55b74eb6003d98
Nastasya <kateanger@yandex.ru>
2023-05-27 19:41:56 +0300

    -feat перенесла проверку дат букинга в шлюз -style заменила проверку на .isEmpty()

--- gateway/src/main/java/ru/practicum/shareit/exception/WrongDatesException.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/WrongDatesException.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.exception;
 
-public class WrongDatesException extends RuntimeException{
+public class WrongDatesException extends RuntimeException {
     public WrongDatesException(String message) {
         super(message);
     }
